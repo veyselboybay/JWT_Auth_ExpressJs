6cbcdee26326288e34f0ec84a4e6dd15e1a0c17c
veyselboybay <veysel.boybay@outlook.com>
2022-08-11 17:34:04 -0400

    MongoDB connection successfull

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { urlencoded } = require('express');
 const express = require('express')
 const authRoute = require('./routes/auth');
-
+const mongoose = require('mongoose')
+require('dotenv/config')
 
 // Create express app
 const app = express()
@@ -13,6 +13,15 @@ app.use(express.urlencoded({extended:false}));
 // Middleware for routes
 app.use('/api/user',authRoute);
 
+
+
+// DB connection with mongoDB
+mongoose.connect(process.env.DB_CONNECTION,(err)=>{
+    if(err){
+        return console.log(err);
+    }
+    console.log('Database connected successfully...');
+})  
 
 
 // Listen server
