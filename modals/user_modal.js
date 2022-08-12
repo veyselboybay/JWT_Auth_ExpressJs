d7969ae7aebc4a02eb298a1d7fba1bc1231d2c0d
veyselboybay <veysel.boybay@outlook.com>
2022-08-11 22:53:21 -0400

    joi validation installed

---
+++ modals/user_modal.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        require:true,
+        min:6
+    },
+    email:{
+        type:String,
+        required:true,
+        min:6
+    },
+    password:{
+        type:String,
+        required:true,
+        max:1024,
+        min:6
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+module.exports = mongoose.model('User',userSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,35 @@
 const router = require('express').Router();
+const User = require('../modals/user_modal');
+
+//Validation
+const joi = require('@hapi/joi')
+
+const schema = joi.object({
+    name: joi.string().min(6).required(),
+    email:joi.string().min(6).required().email(),
+    password:joi.string().min(6).required()
+})
 
 /**
  * METHOD: POST
  * PURPOSE: REgister a new user to database
  */
 router.post('/register',async (req,res) =>{
-    res.send(req.body)
+    const reqBody = req.body;
+    //Validate data
+    const validation = schema.validate(req.body);
+    res.send(validation);
+    // const user = new User({
+    //     name:reqBody.name,
+    //     email:reqBody.email,
+    //     password:reqBody.password,
+    // });
+    // try{
+    //     const isOk = await user.save();
+    //     return res.status(200).send(isOk);
+    // }catch(err){
+    //     return res.status(400).send(err);
+    // }
 });
 
 /**
